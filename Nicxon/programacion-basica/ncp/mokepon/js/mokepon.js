b0f9fc5098c94bee36e5e801c53c11d4fd3416e9
NicxonO <nicxon_ospina@soy.sena.edu.co>
2023-04-10 19:10:32 -0500

    Feat: Agregacion de escuchador de eventos para carga del HTML

--- Nicxon/programacion-basica/ncp/mokepon/js/mokepon.js
+++ Nicxon/programacion-basica/ncp/mokepon/js/mokepon.js
@@ -1,5 +1,20 @@
-function seleccionarMascotaJugador() {
-    alert('seleccionaste tu mascota')
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota');
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+
 }
-let botonMascotaJugador = document.getElementById('boton-mascota');
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+function seleccionarMascotaJugador() {
+    
+    if (document.getElementById('hipodoge').checked){
+        alert('seleccionaste tu al hipodoge');
+    }
+    else if (document.getElementById('capipepo').checked){
+        alert('seleccionaste tu al capipepo');
+    }
+    else if (document.getElementById('ratigueya').checked){
+        alert('seleccionaste tu a la ratigueya');
+    }
+}
+
+window.addEventListener('load', iniciarJuego);
