740d4b0a32d9c8e3a73d4d96dbe9361021dfa1db
NicxonO <nicxon_ospina@soy.sena.edu.co>
2023-05-08 12:21:19 -0500

    Actualización clase 64

--- Nicxon/programacion-basica/ncp/mokepon/js/mokepon.js
+++ Nicxon/programacion-basica/ncp/mokepon/js/mokepon.js
@@ -115,8 +115,7 @@ function iniciarJuego(){
 function llamarSectionAtaque() {
     //sectionSeleccionarAtaque.style.display = 'flex';
     SECTION_VER_MAPA.style.display = 'flex';
-    intervalo = setInterval(pintarPersonaje,50);
-
+    iniciarMapa();
 }
 
 function seleccionarMascotaJugador() {
@@ -301,5 +300,28 @@ function moverArriba(){
 function detenerMovimiento(){
     capipepo.velocidadX = 0;
     capipepo.velocidadY = 0;
+}
+function sePresionoUnaTecla(event){
+    switch (event.key){
+        case 'ArrowUp':
+            moverArriba();
+            break;
+        case 'ArrowDown':
+            moverAbajo();
+            break;
+        case 'ArrowLeft':
+            moverIzquierda();
+            break;
+        case 'ArrowRight':
+            moverDerecha();
+            break;
+        default:
+            break;
+    }
+}
+function iniciarMapa(){
+    intervalo = setInterval(pintarPersonaje,50);
+    window.addEventListener('keydown', sePresionoUnaTecla);
+    window.addEventListener('keyup', detenerMovimiento);
 }
 window.addEventListener('load', iniciarJuego);
