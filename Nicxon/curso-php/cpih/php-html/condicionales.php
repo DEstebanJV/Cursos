40bc66cb5ebee29ca7f4caa8cdb744e77bc5d557
NicxonO <nicxon_ospina@soy.sena.edu.co>
2023-11-10 22:08:35 -0500

    Feat: Add all worknotes from ES course

---
+++ Nicxon/curso-php/cpih/php-html/condicionales.php
@@ -0,0 +1,26 @@
+<?php
+$se_habla_de_bruno = true;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Condicionales</title>
+</head>
+<body>
+    
+    <?phpif ($se_hablo_de_bruno) { ?>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQocJ5pZ_TN9hVhQiFLAK21u1ISHheOxvaYKWtkmmXelYqTFMQfJThUJ_jvovRKaKnn79I&usqp=CAU'alt='bruno sorprendido'/>
+    
+    <?php }else { ?>
+
+        <img src='https://studiosol-a.akamaihd.net/letras/215x215/fotos/e/0/a/f/e0af6a91973bbef6efcd9a426414c910.jpg'alt='bruno contento'/>
+    
+    <?php } ?>
+
+</body>
+</html>
